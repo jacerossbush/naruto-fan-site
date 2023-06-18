d62318dc1371b541dee9f4ce96edda9d92fc9e0e
Jace Bush <jacerossbush@gmail.com>
2023-06-17 21:08:11 -0600

    add use effect and import api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import { useEffect, useState } from 'react';
 import './index.css';
 
+
 function App() {
+
+  const[data, setData] = useState({});
+
+  // useEffect(() => {
+  //   fetch('https://api.jikan.moe/v4/characters/17/full')
+  //     .then(response => response.json())
+  //     .then(json => setData(json))
+  //     .catch(error => console.error(error));
+  // }, []);
+  // console.log(data.data.images.jpg.image_url);
   return (
     <div className="App">
+      <h1>Naruto facts</h1>
+      {/* <img src={data.data["images"].jpg.image_url} alt="" /> */}
+      
 
     </div>
   );

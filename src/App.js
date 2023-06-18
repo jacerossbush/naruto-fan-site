e8e7b083bafdadc5b9c0cc8bb0f35dc26a65ab38
Jace Bush <jacerossbush@gmail.com>
2023-06-18 15:11:08 -0600

    add dynamic changing to useeffect &error handling

--- src/App.js
+++ src/App.js
@@ -5,17 +5,18 @@ import './index.css';
 function App() {
 
   const[anime, setAnime] = useState({});
+  const[characterId, setCharacterId] = useState(17);
 
   useEffect(() => {
-    fetch('https://api.jikan.moe/v4/characters/17/full')
+    fetch(`https://api.jikan.moe/v4/characters/${characterId}/full`)
       .then(response => response.json())
       .then(json => setAnime(json))
       .catch(error => console.error(error));
-  }, []);
+  }, [characterId]);
 
-  const animeImage = anime.data.images.jpg.image_url;
-  const animeName = anime.data.name;
-  const animeAbout = anime.data.about;
+  const animeImage = anime.data?.images?.jpg?.image_url;
+  const animeName = anime.data?.name;
+  const animeAbout = anime.data?.about;
   console.log(anime);
   return (
     <div className="App">

5575994561fc2324de2bd9d4894d6289b0dca53e
Jace Bush <jacerossbush@gmail.com>
2023-06-17 23:04:43 -0600

    fix useeffect

--- src/App.js
+++ src/App.js
@@ -6,17 +6,17 @@ function App() {
 
   const[data, setData] = useState({});
 
-  // useEffect(() => {
-  //   fetch('https://api.jikan.moe/v4/characters/17/full')
-  //     .then(response => response.json())
-  //     .then(json => setData(json))
-  //     .catch(error => console.error(error));
-  // }, []);
-  // console.log(data.data.images.jpg.image_url);
+  useEffect(() => {
+    fetch('https://api.jikan.moe/v4/characters/17/full')
+      .then(response => response.json())
+      .then(json => setData(json))
+      .catch(error => console.error(error));
+  }, []);
+  console.log(data);
   return (
     <div className="App">
       <h1>Naruto facts</h1>
-      {/* <img src={data.data["images"].jpg.image_url} alt="" /> */}
+      <img src={data.data.images.jpg.image_url} alt="" />
       
 
     </div>

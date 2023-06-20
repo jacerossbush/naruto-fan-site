76b385c77c920172ff0355263a0efcbec8382f48
Jace Bush <jacerossbush@gmail.com>
2023-06-20 14:51:02 -0600

    finish characterlist array

--- src/CharacterList.js
+++ src/CharacterList.js
@@ -1,24 +1,864 @@
 const CharacterList = [
     {
-        name: "Naruto Uzumaki",
-        id: 17
-    },
-    {
-        name: "Itachi Uchiha",
-        id: 14
-    },
-    {
-        name: "Sakura Haruno",
-        id: 145 
-    },
-    {
-        name: "Sasuke Uchiha",
-        id: 13
-    },
-    {
-        name: "Kakashi Hatake",
+        name: "Haruno, Sakura",
+        id: 145
+        },
+        {
+        name: "Hatake, Kakashi",
         id: 85
-    }
+        },
+        {
+        name: "Uchiha, Sasuke",
+        id: 13
+        },
+        {
+        name: "Uzumaki, Naruto",
+        id: 17
+        },
+        {
+        name: "Abumi, Zaku",
+        id: 8997
+        },
+        {
+        name: "Aburame, Shino",
+        id: 3428
+        },
+        {
+        name: "Aburame, Shibi",
+        id: 10697
+        },
+        {
+        name: "Akado, Manabu",
+        id: 17538
+        },
+        {
+        name: "Akado, Yoroi",
+        id: 9699
+        },
+        {
+        name: "Akahoshi",
+        id: 10698
+        },
+        {
+        name: "Akamaru",
+        id: 4799
+        },
+        {
+        name: "Akame, Iwana",
+        id: 149421
+        },
+        {
+        name: "Akane",
+        id: 20645
+        },
+        {
+        name: "Akimichi, Chouji",
+        id: 2008
+        },
+        {
+        name: "Akimichi, Chouza",
+        id: 10699
+        },
+        {
+        name: "Akio",
+        id: 10700
+        },
+        {
+        name: "Ami",
+        id: 23006
+        },
+        {
+        name: "Ayame",
+        id: 17310
+        },
+        {
+        name: "Baiu",
+        id: 23081
+        },
+        {
+        name: "Baki",
+        id: 15081
+        },
+        {
+        name: "Bekkou",
+        id: 149422
+        },
+        {
+        name: "Bisuke",
+        id: 34075
+        },
+        {
+        name: "Chishima",
+        id: 17165
+        },
+        {
+        name: "Deidara",
+        id: 1902
+        },
+        {
+        name: "Dengaku",
+        id: 17537
+        },
+        {
+        name: "Ebisu",
+        id: 10791
+        },
+        {
+        name: "Enma",
+        id: 15757
+        },
+        {
+        name: "Fukuyokana",
+        id: 160858
+        },
+        {
+        name: "Fuujin",
+        id: 206143
+        },
+        {
+        name: "Fuuma, Kamikiri",
+        id: 15176
+        },
+        {
+        name: "Fuuma, Arashi",
+        id: 17316
+        },
+        {
+        name: "Fuuma, Kotohime",
+        id: 27688
+        },
+        {
+        name: "Fuuma, Sasame",
+        id: 15177
+        },
+        {
+        name: "Fuuma, Jigumo",
+        id: 17313
+        },
+        {
+        name: "Gaara",
+        id: 1662
+        },
+        {
+        name: "Gamabunta",
+        id: 4223
+        },
+        {
+        name: "Gamakichi",
+        id: 16999
+        },
+        {
+        name: "Gamatatsu",
+        id: 16952
+        },
+        {
+        name: "Gantetsu",
+        id: 83755
+        },
+        {
+        name: "Gatou",
+        id: 63793
+        },
+        {
+        name: "Gatsu",
+        id: 17536
+        },
+        {
+        name: "Gekkou, Hayate",
+        id: 7738
+        },
+        {
+        name: "Genno",
+        id: 17199
+        },
+        {
+        name: "Genyuumaru",
+        id: 22922
+        },
+        {
+        name: "Genzou",
+        id: 17535
+        },
+        {
+        name: "Giichi",
+        id: 20274
+        },
+        {
+        name: "Gouzu",
+        id: 16164
+        },
+        {
+        name: "Guruko",
+        id: 23569
+        },
+        {
+        name: "Hachidai",
+        id: 20278
+        },
+        {
+        name: "Hagane, Kotetsu",
+        id: 16304
+        },
+        {
+        name: "Hakkaku",
+        id: 20279
+        },
+        {
+        name: "Haku",
+        id: 2039
+        },
+        {
+        name: "Haku's Mother",
+        id: 88313
+        },
+        {
+        name: "Hanzaki",
+        id: 227899
+        },
+        {
+        name: "Haruna",
+        id: 15178
+        },
+        {
+        name: "Haruno, Mebuki",
+        id: 67289
+        },
+        {
+        name: "Hijiri, Shimon",
+        id: 23155
+        },
+        {
+        name: "Hikyakuya, Fukusuke",
+        id: 17307
+        },
+        {
+        name: "Hokushin",
+        id: 17424
+        },
+        {
+        name: "Hokuto",
+        id: 17539
+        },
+        {
+        name: "Hoshigaki, Kisame",
+        id: 2672
+        },
+        {
+        name: "Houki",
+        id: 227900
+        },
+        {
+        name: "Hyuuga, Hiashi",
+        id: 15039
+        },
+        {
+        name: "Hyuuga, Hinata",
+        id: 1555
+        },
+        {
+        name: "Hyuuga, Hanabi",
+        id: 12563
+        },
+        {
+        name: "Hyuuga, Neji",
+        id: 1694
+        },
+        {
+        name: "Hyuuga, Hizashi",
+        id: 22880
+        },
+        {
+        name: "Ibara",
+        id: 149423
+        },
+        {
+        name: "Inari",
+        id: 21365
+        },
+        {
+        name: "Inuzuka, Kiba",
+        id: 3495
+        },
+        {
+        name: "Inuzuka, Tsume",
+        id: 15074
+        },
+        {
+        name: "Inuzuka, Hana",
+        id: 15073
+        },
+        {
+        name: "Isaribi",
+        id: 5737
+        },
+        {
+        name: "Ise, Udon",
+        id: 23052
+        },
+        {
+        name: "Jantou",
+        id: 20276
+        },
+        {
+        name: "Jiraiya",
+        id: 2423
+        },
+        {
+        name: "Jiroubou",
+        id: 8495
+        },
+        {
+        name: "Kagari",
+        id: 23056
+        },
+        {
+        name: "Kagero",
+        id: 206141
+        },
+        {
+        name: "Kagetsu, Futa",
+        id: 227807
+        },
+        {
+        name: "Kagetsu, Toubei",
+        id: 227808
+        },
+        {
+        name: "Kagetsu, Tsukiko",
+        id: 227809
+        },
+        {
+        name: "Kagetsu, Mai",
+        id: 227810
+        },
+        {
+        name: "Kaiza",
+        id: 22996
+        },
+        {
+        name: "Kamizuki, Izumo",
+        id: 14032
+        },
+        {
+        name: "Kamizuru, Jibachi",
+        id: 17557
+        },
+        {
+        name: "Kamizuru, Suzumebachi",
+        id: 197867
+        },
+        {
+        name: "Kamizuru, Kurobachi",
+        id: 197866
+        },
+        {
+        name: "Kankurou",
+        id: 4694
+        },
+        {
+        name: "Karashi",
+        id: 17302
+        },
+        {
+        name: "Karura",
+        id: 22248
+        },
+        {
+        name: "Katou, Dan",
+        id: 22876
+        },
+        {
+        name: "Katsuyu",
+        id: 7608
+        },
+        {
+        name: "Kazabune",
+        id: 17544
+        },
+        {
+        name: "Kazamatsuri, Moegi",
+        id: 13454
+        },
+        {
+        name: "Kidoumaru",
+        id: 8497
+        },
+        {
+        name: "Kikunojou",
+        id: 227811
+        },
+        {
+        name: "Kimimaro",
+        id: 3438
+        },
+        {
+        name: "Kinuta, Dosu",
+        id: 9244
+        },
+        {
+        name: "Koumei",
+        id: 206144
+        },
+        {
+        name: "Kujaku",
+        id: 91137
+        },
+        {
+        name: "Kunihisa",
+        id: 43923
+        },
+        {
+        name: "Kunijirou",
+        id: 43924
+        },
+        {
+        name: "Kurama",
+        id: 7407
+        },
+        {
+        name: "Kurama, Unkai",
+        id: 129493
+        },
+        {
+        name: "Kurama, Yakumo",
+        id: 17545
+        },
+        {
+        name: "Kuromaru",
+        id: 22881
+        },
+        {
+        name: "Kurosuki, Raiga",
+        id: 8771
+        },
+        {
+        name: "Kusabi",
+        id: 20277
+        },
+        {
+        name: "Lee, Rock",
+        id: 306
+        },
+        {
+        name: "Manda",
+        id: 37929
+        },
+        {
+        name: "Matsuri",
+        id: 7310
+        },
+        {
+        name: "Matsuri",
+        id: 43972
+        },
+        {
+        name: "Meizu",
+        id: 16165
+        },
+        {
+        name: "Menma",
+        id: 8810
+        },
+        {
+        name: "Midare",
+        id: 23083
+        },
+        {
+        name: "Might, Guy",
+        id: 307
+        },
+        {
+        name: "Mitarashi, Anko",
+        id: 809
+        },
+        {
+        name: "Mitokado, Homura",
+        id: 22648
+        },
+        {
+        name: "Mizura",
+        id: 15759
+        },
+        {
+        name: "Momiji",
+        id: 206139
+        },
+        {
+        name: "Momochi, Zabuza",
+        id: 728
+        },
+        {
+        name: "Mondai",
+        id: 17540
+        },
+        {
+        name: "Morino, Idate",
+        id: 31197
+        },
+        {
+        name: "Morino, Ibiki",
+        id: 22923
+        },
+        {
+        name: "Mubi",
+        id: 16974
+        },
+        {
+        name: "Nagare",
+        id: 17541
+        },
+        {
+        name: "Namiashi, Raidou",
+        id: 17298
+        },
+        {
+        name: "Namikaze, Minato",
+        id: 2535
+        },
+        {
+        name: "Nan",
+        id: 17543
+        },
+        {
+        name: "Nanafushi",
+        id: 17542
+        },
+        {
+        name: "Nara, Shikamaru",
+        id: 2007
+        },
+        {
+        name: "Nara, Yoshino",
+        id: 22247
+        },
+        {
+        name: "Nara, Shikaku",
+        id: 18056
+        },
+        {
+        name: "Natsuhi",
+        id: 77693
+        },
+        {
+        name: "Nawaki",
+        id: 22884
+        },
+        {
+        name: "Oboro",
+        id: 17619
+        },
+        {
+        name: "Oki",
+        id: 16975
+        },
+        {
+        name: "Orochimaru",
+        id: 2455
+        },
+        {
+        name: "Pakkun",
+        id: 2766
+        },
+        {
+        name: "Raijin",
+        id: 206142
+        },
+        {
+        name: "Ran",
+        id: 50957
+        },
+        {
+        name: "Ranmaru",
+        id: 17414
+        },
+        {
+        name: "Rasa",
+        id: 22920
+        },
+        {
+        name: "Renga",
+        id: 17425
+        },
+        {
+        name: "Rokushou, Aoi",
+        id: 6598
+        },
+        {
+        name: "Ruiga",
+        id: 206145
+        },
+        {
+        name: "Ryuugan",
+        id: 17305
+        },
+        {
+        name: "Sagi",
+        id: 23215
+        },
+        {
+        name: "Sakon",
+        id: 8760
+        },
+        {
+        name: "Sanjurou, Shura",
+        id: 227812
+        },
+        {
+        name: "Sanshou",
+        id: 161288
+        },
+        {
+        name: "Sarutobi, Hiruzen",
+        id: 7571
+        },
+        {
+        name: "Sarutobi, Asuma",
+        id: 4775
+        },
+        {
+        name: "Sarutobi, Konohamaru",
+        id: 3889
+        },
+        {
+        name: "Sazanami",
+        id: 17546
+        },
+        {
+        name: "Seimei",
+        id: 10524
+        },
+        {
+        name: "Senju, Tobirama",
+        id: 12465
+        },
+        {
+        name: "Senju, Hashirama",
+        id: 12464
+        },
+        {
+        name: "Senta",
+        id: 17547
+        },
+        {
+        name: "Shibire",
+        id: 23156
+        },
+        {
+        name: "Shigure",
+        id: 15179
+        },
+        {
+        name: "Shin'emon",
+        id: 17548
+        },
+        {
+        name: "Shinayakana",
+        id: 160859
+        },
+        {
+        name: "Shiore",
+        id: 23153
+        },
+        {
+        name: "Shiranui, Genma",
+        id: 3735
+        },
+        {
+        name: "Shizune",
+        id: 4643
+        },
+        {
+        name: "Shukaku",
+        id: 11813
+        },
+        {
+        name: "Sumaru",
+        id: 82609
+        },
+        {
+        name: "Suzume",
+        id: 23154
+        },
+        {
+        name: "Tanzou",
+        id: 17551
+        },
+        {
+        name: "Tatami, Iwashi",
+        id: 23759
+        },
+        {
+        name: "Tayuya",
+        id: 3052
+        },
+        {
+        name: "Tazuna",
+        id: 17197
+        },
+        {
+        name: "Temari",
+        id: 2174
+        },
+        {
+        name: "Tenten",
+        id: 3710
+        },
+        {
+        name: "Teuchi",
+        id: 17312
+        },
+        {
+        name: "Tobio",
+        id: 24324
+        },
+        {
+        name: "Tobitake, Tonbo",
+        id: 23140
+        },
+        {
+        name: "Todoroki",
+        id: 17200
+        },
+        {
+        name: "Toki",
+        id: 16321
+        },
+        {
+        name: "Tonton",
+        id: 90849
+        },
+        {
+        name: "Touji, Mizuki",
+        id: 19253
+        },
+        {
+        name: "Tsuba",
+        id: 23145
+        },
+        {
+        name: "Tsubaki",
+        id: 17666
+        },
+        {
+        name: "Tsubaki",
+        id: 149424
+        },
+        {
+        name: "Tsuchi, Kin",
+        id: 9680
+        },
+        {
+        name: "Tsunade",
+        id: 2767
+        },
+        {
+        name: "Tsunami",
+        id: 15077
+        },
+        {
+        name: "Tsurugi, Misumi",
+        id: 22921
+        },
+        {
+        name: "Uchiha, Teyaki",
+        id: 23143
+        },
+        {
+        name: "Uchiha, Inabi",
+        id: 22891
+        },
+        {
+        name: "Uchiha, Yashiro",
+        id: 22908
+        },
+        {
+        name: "Uchiha, Uruchi",
+        id: 23144
+        },
+        {
+        name: "Uchiha, Fugaku",
+        id: 16973
+        },
+        {
+        name: "Uchiha, Tekka",
+        id: 22889
+        },
+        {
+        name: "Uchiha, Itachi",
+        id: 14
+        },
+        {
+        name: "Uchiha, Mikoto",
+        id: 21122
+        },
+        {
+        name: "Ukon",
+        id: 41366
+        },
+        {
+        name: "Umino, Iruka",
+        id: 2011
+        },
+        {
+        name: "Utatane, Koharu",
+        id: 22649
+        },
+        {
+        name: "Uzuki, Yugao",
+        id: 15045
+        },
+        {
+        name: "Waraji",
+        id: 17552
+        },
+        {
+        name: "Wasabi, Jirouchou",
+        id: 16976
+        },
+        {
+        name: "Yajirobee",
+        id: 17549
+        },
+        {
+        name: "Yakushi, Kabuto",
+        id: 2405
+        },
+        {
+        name: "Yamanaka, Inoichi",
+        id: 15072
+        },
+        {
+        name: "Yamanaka, Ino",
+        id: 2009
+        },
+        {
+        name: "Yamashiro, Aoba",
+        id: 17553
+        },
+        {
+        name: "Yashamaru",
+        id: 13017
+        },
+        {
+        name: "Yotaka",
+        id: 15145
+        },
+        {
+        name: "Yurinojou",
+        id: 227898
+        },
+        {
+        name: "Yuuhi, Kurenai",
+        id: 4773
+        },
+        {
+        name: "Zetsu",
+        id: 3150
+        },
+        {
+        name: "Zouri",
+        id: 22931
+        }
 ]
 
 export default CharacterList;

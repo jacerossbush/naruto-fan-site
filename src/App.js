dea3982111a0b3b9bd6e7227857274e11b6a9fca
Jace Bush <jacerossbush@gmail.com>
2023-06-19 18:26:44 -0600

    add second useeffect to get character array

--- src/App.js
+++ src/App.js
@@ -4,26 +4,48 @@ import './index.css';
 
 function App() {
 
-  const[anime, setAnime] = useState({});
+  const[characterList, setCharacterList] = useState({});
+
+  const[character, setCharacter] = useState({});
   const[characterId, setCharacterId] = useState(17);
+
+  useEffect(() => {
+    fetch(`https://api.jikan.moe/v4/anime/20/characters`)
+      .then(response => response.json())
+      .then(json => setCharacterList(json))
+      .catch(error => console.error(error));
+  }, []);
 
   useEffect(() => {
     fetch(`https://api.jikan.moe/v4/characters/${characterId}/full`)
       .then(response => response.json())
-      .then(json => setAnime(json))
+      .then(json => setCharacter(json))
       .catch(error => console.error(error));
   }, [characterId]);
 
-  const animeImage = anime.data?.images?.jpg?.image_url;
-  const animeName = anime.data?.name;
-  const animeAbout = anime.data?.about;
-  console.log(anime);
+  const characters = characterList?.data;
+  
+
+  const characterImage = character.data?.images?.jpg?.image_url;
+  const characterName = character.data?.name;
+  const characterAbout = character.data?.about;
+  console.log(characterList)
+  console.log(characters);
   return (
     <div className="App">
       <h1>Naruto facts</h1>
-      <img src={animeImage} alt="" />
-      <h2>{animeName}</h2>
-      <p>{animeAbout}</p>
+      <img src={characterImage} alt="" />
+      <h2>{characterName}</h2>
+      <p>{characterAbout}</p>
+      <button onClick={() => setCharacterId(characterId === 14 ? 17 : 14)}>Change characters</button>     
+       {/* <h1>{characters[0].character.mal_id}</h1> */}
+       {characters.map(item => (
+        <>
+          <h3>{item.character.name}</h3>
+        </>
+
+       ))}
+      
       
 
     </div>

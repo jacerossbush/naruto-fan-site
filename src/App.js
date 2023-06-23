ff30aa0eb34c59a12c859b75bf2014d12b6a470c
Jace Bush <jacerossbush@gmail.com>
2023-06-22 22:10:47 -0600

    remove unused import

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import { useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
